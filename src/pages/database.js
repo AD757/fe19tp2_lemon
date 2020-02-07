20b87e7cd6a3e72f3a021ba0fba2927939c78abf
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-02-08 00:08:49 +0100

    Update database.js

--- src/pages/database.js
+++ src/pages/database.js
@@ -4,7 +4,7 @@ export default class database extends Component {
     render() {
         return (
             <div>
-                DATABASE
+                <p>DATABASE</p>
             </div>
         )
     }

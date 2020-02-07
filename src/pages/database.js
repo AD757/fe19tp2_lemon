f0c2a18926b342b16fa0baf57c4763cceac1bad1
ClaesPhilip <claesphiliplofgren@hotmail.com>
2020-02-07 23:30:45 +0100

    Update database.js

---
+++ src/pages/database.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class database extends Component {
+    render() {
+        return (
+            <div>
+                DATABASE
+            </div>
+        )
+    }
+}

19145ed6963521914dd7360ef695b4a825160300
ClaesPhilip <claesphiliplofgren@hotmail.com>
2020-02-07 23:30:48 +0100

    Update food.js

---
+++ src/pages/food.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class food extends Component {
+    render() {
+        return (
+            <div>
+                FOOD
+            </div>
+        )
+    }
+}

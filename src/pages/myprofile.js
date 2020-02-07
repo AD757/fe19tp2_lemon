8054f0de6aae8c5496f0d052d09c180ba6a1b32b
ClaesPhilip <claesphiliplofgren@hotmail.com>
2020-02-07 23:30:50 +0100

    Update myprofile.js

---
+++ src/pages/myprofile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class myprofile extends Component {
+    render() {
+        return (
+            <div>
+                MY PROFILE
+            </div>
+        )
+    }
+}

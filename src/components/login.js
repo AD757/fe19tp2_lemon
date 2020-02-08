89740390124e40373bfa611668f1191054a2ec28
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-02-08 18:08:23 +0100

    Revert "Revert "Create login.js""
    This reverts commit 90cfeee66b052eded2bd8aeedac51745f736186d.

---
+++ src/components/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class login extends Component {
+    render() {
+        return (
+            <div>
+                login
+            </div>
+        )
+    }
+}

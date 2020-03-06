fcac4f523e85f41b19187b3ba2100d24ee30c3e2
juiceghost <jg@renegade.se>
2020-03-06 15:01:27 +0100

    Tweaked .bind

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -11,79 +11,88 @@ const myFatAPI = new fatAPI(key, secret);
 
 const dummyData = {
     labels: ['Carbohydrates', 'Protein', 'Fat',],
-    datasets: 
-      [{
-        text: undefined, 
-        label: 'Rainfall',
-        backgroundColor: [
-          '#B21F00',
-          '#C9DE00',
-          '#2FDE00',
-        ],
-        hoverBackgroundColor: [
-        '#501800',
-        '#4B5000',
-        '#175000',
-        ],
-        data: [undefined]
+    datasets:
+        [{
+            text: undefined,
+            label: 'Rainfall',
+            backgroundColor: [
+                '#B21F00',
+                '#C9DE00',
+                '#2FDE00',
+            ],
+            hoverBackgroundColor: [
+                '#501800',
+                '#4B5000',
+                '#175000',
+            ],
+            data: [undefined]
         }]
-      }
+}
 
 class Main extends Component {
-    state={
-        
+    constructor(props) {
+        super(props);
+        this.state = {
+            pieData: {}
+        }
+        this.ItemClick = this.ItemClick.bind(this);
+        this.saveFoodData = this.saveFoodData.bind(this);
     }
-
-    ItemClick (e) {
-        const target = e.target;
-        myFatAPI.method('food.get', {
-            food_id: target.id
-        })
-        .then(identifiedItem => {
-            console.log(identifiedItem)
-           let servingsIdentifier = identifiedItem.food.servings.serving[0];
-           if(!servingsIdentifier){
-              servingsIdentifier = identifiedItem.food.servings.serving;
-           }
-           this.setState(state => {
-              state.itemDetails = {
-                 value: target.value,
-                 food_name: identifiedItem.food.food_name,
-                 calories: servingsIdentifier.calories,
-                 carbohydrates: servingsIdentifier.carbohydrate,
-                 fat: servingsIdentifier.fat, //grams
-                 protein: servingsIdentifier.protein, //grams
-                 amount: servingsIdentifier.metric_serving_amount,
-                 unit: servingsIdentifier.metric_serving_unit
-              }
-  
-              state.pieData.text = identifiedItem.food.food_name;
-              state.pieData.data = [servingsIdentifier.carbohydrate, servingsIdentifier.protein, servingsIdentifier.fat];
-           })
-           this.props.saveFoodData(this.state.pieData, this.state.itemDetails);
-        })
-     }
 
     saveFoodData = (foodData, itemDetails) => {
         console.log(itemDetails);
         this.setState({ foodData, itemDetails })
     }
-
-    render() {  
-        if (this.state.foodData) {
-        dummyData.datasets[0].data = this.state.foodData.data;
+    ItemClick = (e) => {
+        console.log(this)
+        const target = e.target;
+        console.log("ID: " + e.target.id)
+        myFatAPI.method('food.get', {
+            food_id: target.id
+        })
+            .then(identifiedItem => {
+                console.log(identifiedItem)
+                let servingsIdentifier = identifiedItem.food.servings.serving[0];
+                console.log(servingsIdentifier)
+                if (!servingsIdentifier) {
+                    servingsIdentifier = identifiedItem.food.servings.serving;
+                }
+                console.log(servingsIdentifier)
+                this.setState(state => {
+                    state.itemDetails = {
+                        value: target.value,
+                        food_name: identifiedItem.food.food_name,
+                        calories: servingsIdentifier.calories,
+                        carbohydrates: servingsIdentifier.carbohydrate,
+                        fat: servingsIdentifier.fat, //grams
+                        protein: servingsIdentifier.protein, //grams
+                        amount: servingsIdentifier.metric_serving_amount,
+                        unit: servingsIdentifier.metric_serving_unit
+                    }
+                    state.pieData.text = identifiedItem.food.food_name;
+                    state.pieData.data = [servingsIdentifier.carbohydrate, servingsIdentifier.protein, servingsIdentifier.fat];
+                })
+                this.saveFoodData(this.state.pieData, this.state.itemDetails);
+            })
     }
 
-    return (
-        <div className = "mainContent"> 
-            {this.state.foodData ? <div className = "graphContainer">
-                <Charts data={dummyData} text={this.state.foodData.text} foodInfo={this.state.itemDetails}/>
-                </div> : null }
-            <Search saveFoodData={this.saveFoodData} ItemClick={this.ItemClick} />
-            <TopTenFoods ItemClick={this.ItemClick}/>
-        </div>
-    );
-}
+
+
+    render() {
+        if (this.state.foodData) {
+            dummyData.datasets[0].data = this.state.foodData.data;
+        }
+
+        return (
+            <div className="mainContent">
+                {this.state.foodData ? <div className="graphContainer">
+                    <Charts data={dummyData} text={this.state.foodData.text} foodInfo={this.state.itemDetails} />
+                </div> : null}
+                <Search saveFoodData={this.saveFoodData} ItemClick={this.ItemClick} />
+                <TopTenFoods ItemClick={this.ItemClick} />
+            </div>
+        );
+    }
 }
 
 export default Main;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styles.css';
 import fatAPI from '../API';
-import {FaSearch} from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 const key = '0a656d2e1070441d81b256544a739083';
 const secret = 'fb94850a73f9420793fe67bb98c77b41';
 const myFatAPI = new fatAPI(key, secret);
 
 class Search extends React.Component {
-   constructor (props) {
+   constructor(props) {
       super(props);
       this.searchForFood = this.searchForFood.bind(this);
       this.searchRef = React.createRef();
@@ -21,12 +21,12 @@ class Search extends React.Component {
          itemDetails: [],
          pieData: {
             text: "hello",
-            data: [1,1,1]
+            data: [1, 1, 1]
          }
       }
    }
 
-   searchForFood (e) {
+   searchForFood(e) {
       e.preventDefault();
       //Skapar en temporär array för att lagra sökningarna i innan de ändras i state
       let tempArray = [];
@@ -41,52 +41,53 @@ class Search extends React.Component {
             search_expression: this.state.food_search,
             max_results: 10,
          })
-         .then(results => this.setState({resultsArray: results.foods.food}, () => {
-            console.log(this.state.resultsArray);
-            if(this.state.resultsArray !== undefined){ 
-               tempArray = this.state.resultsArray.map((items, index) => <li 
-                  className = "foodListItem"
-                  value = {index} 
-                  id = {items.food_id} 
-                  key = {items.food_id} 
-                  onClick = {this.props.ItemClick.bind(this)}>{items.food_name}{/* <button>Add</button> */}
+            .then(results => this.setState({ resultsArray: results.foods.food }, () => {
+               console.log(this.state.resultsArray);
+               if (this.state.resultsArray !== undefined) {
+                  tempArray = this.state.resultsArray.map((items, index) => <li
+                     className="foodListItem"
+                     value={index}
+                     id={items.food_id}
+                     key={items.food_id}
+                     onClick={(e) => this.props.ItemClick(e)}>{items.food_name}{/* <button>Add</button> */}
                   </li>)
-               console.log(tempArray);
-            }
-         
-         if(this.state.food_list.length === 0){
-            this.setState(state=>{
-               const food_list = state.food_list.concat(tempArray);
-               console.log(food_list);
-               return {
-                  food_list
+                  console.log(tempArray);
                }
-         })}
-         else if(this.state.food_list.length <= 10){
-            this.setState(state=>{
-               const food_list = state.food_list.map((x, i) => x = tempArray[i])
-               console.log(food_list);
-               return{
-                  food_list
+
+               if (this.state.food_list.length === 0) {
+                  this.setState(state => {
+                     const food_list = state.food_list.concat(tempArray);
+                     console.log(food_list);
+                     return {
+                        food_list
+                     }
+                  })
                }
-            })
-         }
-         }))
+               else if (this.state.food_list.length <= 10) {
+                  this.setState(state => {
+                     const food_list = state.food_list.map((x, i) => x = tempArray[i])
+                     console.log(food_list);
+                     return {
+                        food_list
+                     }
+                  })
+               }
+            }))
       })
    }
 
-   render () {
+   render() {
       return (
          <div className="foodSearchDiv">
             <div className="searchBarDiv">
-               <form onSubmit ={this.searchForFood}>
+               <form onSubmit={this.searchForFood}>
                   <input type="text" id="foodSearchInput" ref={this.searchRef}></input>
                   <FaSearch type="submit" id="foodSearchBtn" onClick={this.searchForFood} />
                </form>
             </div>
-               {this.state.showFoodList ?
-                  <ul className = "foodList">{this.state.food_list}</ul>: null
-               }  
+            {this.state.showFoodList ?
+               <ul className="foodList">{this.state.food_list}</ul> : null
+            }
          </div>
       );
    }
